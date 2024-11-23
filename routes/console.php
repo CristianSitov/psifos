bbd0277568ffb8971bb8b3a84c2f8ecf22bcaf7a
CristianSitov <cristian.sitov@gmail.com>
2024-11-23 23:51:05 +0200

    Update Hours

--- routes/console.php
+++ routes/console.php
@@ -6,4 +6,10 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('app:fetch:hours')
     ->hourlyAt(1);
 Schedule::command('app:fetch:results')
-    ->everyMinute();
+    ->everyMinute()
+    ->between('6:00', '23:00')
+    ->timezone('Europe/Bucharest');
+Schedule::command('app:fetch:results')
+    ->everyFiveMinutes()
+    ->between('23:00', '6:00')
+    ->timezone('Europe/Bucharest');
