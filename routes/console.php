ef22624d7dee11e201f6d2bb97e0c7ac6ce4b370
CristianSitov <cristian.sitov@gmail.com>
2024-11-25 18:37:20 +0200

    Disable cron

--- routes/console.php
+++ routes/console.php
@@ -3,7 +3,7 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('app:fetch:results')
-    ->everyMinute();
-Schedule::command('app:fetch:finals')
-    ->everyMinute();
+//Schedule::command('app:fetch:results')
+//    ->everyMinute();
+//Schedule::command('app:fetch:finals')
+//    ->everyMinute();
