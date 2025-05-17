15ccc82c2c24a270b6079478d31df208e849f0a1
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2025-05-17 20:06:14 +0300

    Fix

--- app/Console/Commands/VotingHours2025.php
+++ app/Console/Commands/VotingHours2025.php
@@ -30,7 +30,7 @@ class VotingHours2025 extends Command
     public function handle(): void
     {
         sleep(random_int(1, 10));
-        logger()->info(date('Y-m-d H:i:s') . ' // Fetching voting results for 2025...');
+        logger()->info(date('Y-m-d H:i:s') . ' // Fetching voting hours for 2025...');
 
 //        $session = 'prezidentiale04052025';
         $session = 'prezidentiale18052025';
--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,6 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('app:fetch:hours:2025')
-    ->hourlyAt(5);
+    ->hourlyAt(0);
 Schedule::command('app:fetch:results:2025')
     ->everyMinute();
