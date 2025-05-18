ef40b1d13c0a15a5f6afd0c48a8ddef8cb69b7b3
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2025-05-18 21:24:48 +0300

    Amend refreshes

--- routes/console.php
+++ routes/console.php
@@ -3,11 +3,9 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('app:fetch:hours:2025')
-    ->hourlyAt(1);
-Schedule::command('app:fetch:results:2025')
-    ->everyMinute();
+//Schedule::command('app:fetch:hours:2025')
+//    ->hourlyAt(1);
+//Schedule::command('app:fetch:results:2025')
+//    ->everyMinute();
 Schedule::command('app:fetch:finals')
-    ->everyMinute()
-    ->between('18:00', '06:00')
-    ->timezone('UTC');
+    ->everyMinute();
